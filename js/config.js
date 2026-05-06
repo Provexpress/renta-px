ec2de80217c3c1f626fe2bc65c27f2ebf0ed270f
Felipe Cárdenas <cardenasdaniel178@gmail.com>
2026-05-06 12:46:35 -0500

    Configure Microsoft app registration

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 const APP_CONFIG = {
   msal: {
-    clientId: "REEMPLAZAR_CLIENT_ID",
-    tenantId: "REEMPLAZAR_TENANT_ID",
-    redirectUri: window.location.origin + window.location.pathname
+    clientId: "cc4ed0ed-1eb1-45a3-82bc-25a8df0f3f03",
+    tenantId: "e6805558-f5bb-444c-8af2-5f3a4d6dd3fc",
+    redirectUri: "https://provexpress.github.io/renta-px/"
   },
   graph: {
     scopes: ["User.Read", "Files.Read.All", "Sites.Read.All"],
